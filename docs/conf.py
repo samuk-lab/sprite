59a83228d67fa87230ed560eee29581ab7b40bd7
Kieran Samuk <ksamuk@gmail.com>
2026-05-29 13:53:49 -0700

    ruff check!

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,12 @@
 from __future__ import annotations
 
 import sys
-import tomllib
 from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
 
 ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(ROOT / "src"))
